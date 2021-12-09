9d997579b0ae65d09f529b00c2e3f8c326006b75
Michael Rainsford Ryan <C00239510@itcarlow.ie>
2021-12-09 11:31:30 +0000

    Added colour changing

--- scripts/Canvas.gd
+++ scripts/Canvas.gd
@@ -1,12 +1,13 @@
 extends Sprite
 
 var image = Image.new()
+var color : Color = Color.black
 
 
 func _ready():
 	texture = ImageTexture.new()
-	image.create(32, 32, false, Image.FORMAT_RGB8)
-	image.fill(Color.blue)
+	image.create(32, 32, false, Image.FORMAT_RGBA8)
+	image.fill(Color.white)
 	texture.create_from_image(image, 0)
 
 
@@ -16,7 +17,7 @@ func _input(event):
 			var mouse_pos = get_local_mouse_position()
 			if (mouse_pos.x < image.get_width() and 
 			   mouse_pos.y < image.get_height()):
-				set_pixel_and_display(mouse_pos, Color.red)
+				set_pixel_and_display(mouse_pos, color)
 
 
 func set_pixel_and_display(pixel_position : Vector2, color : Color):
@@ -24,3 +25,7 @@ func set_pixel_and_display(pixel_position : Vector2, color : Color):
 	image.set_pixelv(pixel_position, color)
 	image.unlock()
 	texture.create_from_image(image, 0)
+
+
+func _on_ColorPickerButton_color_changed(_color):
+	color = _color
